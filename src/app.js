a1d2b17c2e698e2251dd2c280304fc778f7db4d6
Tatsiana Bakhmatovich <tatsiana_bakhmatovich@epam.com>
2020-03-04 11:46:12 +0300

    updates according PR comments

--- src/app.js
+++ src/app.js
@@ -33,15 +33,20 @@ const app = express();
 const PORT = process.execArgv.PORT || 3000;
 
 app.use(express.json());
-app.use('/', logServiceError);
 app.use('/', logService);
 app.use('/users', userRouter);
 app.use('/groups', groupRouter);
+app.use('/', logServiceError);
 
 process.on('uncaughtException', (err, req, res) => {
     logError(err.stack);
     res.status(500).send({ error: 'Server error' });
     process.exit(1);
   });
+
+process.on('unhandledRejection', (err) => {
+    logError(err);
+});
+
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
